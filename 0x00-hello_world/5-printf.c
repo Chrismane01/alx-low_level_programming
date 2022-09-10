4f173cc6f1c933f3072bf06141002612ead6f081
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-10 02:05:55 -0700

    first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: it is the first function of every C program
+ * it is responssible for starting the execution
+ * and termination of the program.
+ * Return: return zero(0)
+ */
+int main(void)
+{
+    printf("with proper grammer, but the outcome is a piece of art,\n");
+    return (0);
+}
+
+
+
+
+
+
+
