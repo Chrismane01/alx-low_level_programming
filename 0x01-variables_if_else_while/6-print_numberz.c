d47a1207b630b5978bc2654564fdb222facae7db
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-11 14:31:20 -0700

    first commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,8 +2,8 @@
 /**
  * main - Entry point
  *
- * Description: This program prints all the 
- * single digit numbers of base 10 starting 
+ * Description: This program prints all the
+ * single digit numbers of base 10 starting
  * zero(0)
  *
  * Return: 0
@@ -16,7 +16,7 @@ int digits;
 
 /* function that prints base ten digits */
 
-for ( digits = '0'; digits <= '9'; digits ++)
+for (digits = '0'; digits <= '9'; digits++)
 
 putchar(digits);
 
