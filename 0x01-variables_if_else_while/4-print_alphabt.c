b2e4a3dd4a477f9e556e9c4b6fd98c9b018ad292
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-12 00:32:15 -0700

    first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabets in lowercase
+ * with the exception of q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+/* variable declaration */
+
+char alpha;
+
+/* function that prints the alphabets in lowercase */
+
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+if (alpha != 'e' && alpha != 'q')
+{
+putchar(alpha);
+}
+}
+putchar('\n');
+
+return (0);
+
+}
