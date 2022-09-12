69e7b91f002e4c2d63c88d61775941b2459b4484
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-12 15:56:54 -0700

    first commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,9 +21,9 @@ for (a = '0'; a < '9'; a++)
 for (b = a + 1; b <= '9'; b++)
 {
 for (c = b + 1; c <= '9'; c++)
-
+{
 if ((b != a) != c)
-
+{
 putchar(a);
 
 putchar(b);
@@ -39,6 +39,7 @@ putchar(',');
 putchar(' ');
 
 }
+
 }
 
 }
