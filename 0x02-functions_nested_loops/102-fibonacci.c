c1407aa9aa9feb398553f874a083c88aa5803bf8
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-14 14:55:14 -0700

    first commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - prints ot first
+ * 50 fibonacci suit numbers
+ * Return: 0
+ */
+int main(void)
+{
+int inc;
+
+unsigned long n1 = 0;
+
+unsigned long n2 = 1;
+
+unsigned long n3;
+
+for (inc = 0; inc < 50; inc++)
+{
+n3 = n1 + n2;
+
+printf("%lu", n3);
+
+n1 = n2;
+
+n2 = n3;
+
+if (inc == 49)
+
+printf("\n");
+
+else
+
+printf(",");
+}
+return (0);
+}
