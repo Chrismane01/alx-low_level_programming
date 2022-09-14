718d091e5297e4734e1096263085ee808e00c9be
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-14 16:00:42 -0700

    first commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,23 +7,18 @@
 int main(void)
 {
 
-long int = a, b, c, d;
+long int a = 1, b = 2;
 
-a = 1;
+long int c = 3;
 
-b = 2;
-
-c = 3
+long int d;
 
 printf("%ld, ", a);
 
 printf("%ld, ", b);
 
-
-
 for (d = 3; d < 50; d++)
 {
-
 
 printf("%ld", c);
 
