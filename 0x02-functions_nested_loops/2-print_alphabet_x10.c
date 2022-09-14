6f6c976845c620f8a94f3cd0430974c0b944c1e3
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-14 07:06:53 -0700

    first commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the abcd ten times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+/* variable declaration */
+
+char c;
+int i;
+
+
+for (i = 0; i < 10; i++)
+{
+for (c = 'a'; c = 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n')
+}
