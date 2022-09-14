e8a8ac83dd54f10051f030a5b09bdb946606c509
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-14 16:37:13 -0700

    first commit

---
+++ 0x02-functions_nested_loops/100-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - this function adds two integers
+ * @a:an integer a
+ * @b:an integer b
+ * Description: Functions that add two integers
+ * return: Result to add and b
+ * 
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
