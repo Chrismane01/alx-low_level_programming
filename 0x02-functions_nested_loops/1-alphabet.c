ffe3163ee27226b5ad2f5e1b592fc288df894bfc
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-14 06:31:36 -0700

    first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - This function prints
+ * abcd - z (lowercase)
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+
+{
+_putchar(c);
+}
+_putchar('\n');
+}
