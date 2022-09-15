3ce6861e20c98121d760cd04e4e0ac73b90aebee
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-15 13:34:51 -0700

    first commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,7 +6,7 @@
 
 void positive_or_negative(int);
 int largest_number(int, int, int);
-void print_remaining_days(int, int, int);
-int convert_day(int, int);
+void convert_day(int month, int day);
+int print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
