d97f1cc3c1946bdc01a58452ab7bc1f18020309f
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-15 07:55:19 -0700

    first commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints positive or negative integer
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+int i;
+
+i = 0;
+
+postive_or_negative(i);
+
+return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+This is for debugging in C Programming Language
