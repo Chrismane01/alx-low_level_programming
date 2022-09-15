5f28dbd501055cea85b59efabac3aac1f6b82c88
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-15 09:51:52 -0700

    0-main.c

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+0x03-debugging; debugging project for C programming
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#nclude <stdio.h>
+
+void positive_or_negative(inti);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
+
+#endif /* MAIN_H */
