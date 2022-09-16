ad4f484adcacb4b68baf4e4760d1562074cf5ee6
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-16 11:11:07 -0700

    first commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * _isupper - functions that checks for uppercase character
+ * isupper - functions that checks for uppercase character
+ * Description: checks for uppercase character
  * @c: character to check
  * Return: 1 if c is uppercase otherwise 0
  */
@@ -11,6 +12,5 @@ if (c >= 'A' && c <= 'Z')
 {
 return (1);
 }
-
 return (0);
 }
