26a7faeea7e74d537641b17e90ab16396d3aa546
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-16 03:06:16 -0700

    first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase letter
+ * @c: character to check
+ *
+ * Return: 0 or 1
+ */
+int isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+xreturn (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+This directory is for functions
+and more nested loops
