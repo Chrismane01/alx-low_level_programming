0f4292dd9f8a8439eb579f9c5f9fea3d651f1480
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-16 12:29:07 -0700

    first commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,21 +9,17 @@
 
 void more_numbers(void)
 {
-char a, b, c, i;
+int i, j = 0;
+  
+for (; j < 10; j++)
+{
+for (i = 0; i <= 14; i++)
+{
+if (c > 9)
+_putchar(i / 10 + '0');
 
-for (i = 0; i < 10; i++)
-{
-for (c = 0; c <= 14; c++)
-{
-if (c < 10)
-b = c;
-else
-{
-a = c / 10;
-b = c % 10;
-_putchar('0' + a);
-}
-_putchar('0' + b);
+_putchar(i % 10 + '0');
 }
 _putchar('\n');
+}
 }
