6a85a36c91b243434c6aab1934942e67516864a2
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-16 12:49:28 -0700

    first commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -10,12 +10,17 @@
 
 int _isdigit(int c)
 {
-if (c >= "0" && c <= "9")
+char i = '0';
+int isdigit = 0;
+  
+for (; i <= '9'; i++)
 {
-return (1);
-}
-else
+if (i == c)
 {
-return (0);
+isdigit = 1;
+break;
 }
+}
+
+return (isdigit);
 }
