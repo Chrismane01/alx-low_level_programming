8acb6278871b33cde9ddf1594b12bfe617f44ca7
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-19 05:16:48 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this function reset the value
+ * of the pointer to 98
+ * @n: this is the pointer to reset
+ * Return: void
+ */
+void reset_to_98(int *n);
+{
+return (*n = 98);
+}
