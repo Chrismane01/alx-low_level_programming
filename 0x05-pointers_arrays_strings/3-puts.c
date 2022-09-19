bcb36ab8fd2b77a97135d7d6fc4ea1fa8274c1fe
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-19 08:12:26 -0700

    first commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,7 +6,6 @@
  * @str: an input string
  * Return: void
  */
-
 void _puts(char *str)
 {
 int i = 0;
@@ -16,5 +15,5 @@ while (str[i] != '0')
 _putchar(str[i]);
 i++;
 }
-_putchar('\n')
+_putchar('\n');
 }
