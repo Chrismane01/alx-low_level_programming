03c39ccc9815409969e33a4c42e2debee6910982
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-19 14:18:08 -0700

    first commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of
- * of a string to stdout
+ * puts2 - prints every other character of a  string to stdout
  * @str: string to print
- *
  * Return: void
  */
 void puts2(char *str)
 {
 int i = 0;
-
 while (*(str + i))
 {
 _putchar(*(str + i));
