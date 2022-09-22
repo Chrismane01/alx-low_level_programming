930bda0d14eef4ace7c55462f2f4739050c8bae4
Chrismane01 <christianigwebuike30@gmail.com>
2022-09-22 12:22:35 -0700

    first commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strcmp - compares two strings
  * @s1: first string
@@ -9,9 +8,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
+
 int i = 0;
 
-while (*(s1 + i) && *(s2 + i) && (*(s1 + i) == *(s2 + i)))
+while (*(s1 + i) && *(s2 + i) &&
+(*(s1 + i) == *(s2 + i)))
 i++;
 return (*(s1 + i) - *(s2 + i));
 }
