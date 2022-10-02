6bce6f9f85fb7434ecc5d4e76a1010b3cc2d0374
Chrismane01 <christianigwebuike30@gmail.com>
2022-10-02 03:00:14 -0700

    first commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * main - prints the name of the arguments passed to it
+ * @argc: argument count
+ * @argv: arguement vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int i = 0;
+
+if (i >= 0)
+{
+printf("%d\n", argc - 1);
+
+}
+return (0);
+}
