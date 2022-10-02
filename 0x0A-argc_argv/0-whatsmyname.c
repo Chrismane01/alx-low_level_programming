fc8fcfa5633167bc7424b9a1e77bc805fc272c24
Chrismane01 <christianigwebuike30@gmail.com>
2022-10-02 01:45:03 -0700

    first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints its name, followed by
+ * a new line
+ * @argc: arguement count
+ * @argv: argument value, a string that come after
+ * calling function
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+int i = 0;
+
+if (argc <= 1)
+{
+printf("%s\n",argv[i]);
+argc--;
+}
+return (0);
+}
