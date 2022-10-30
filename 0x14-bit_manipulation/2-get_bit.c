7150d8ab5764c64a205bafd8275cbac0777ffb66
Chrismane01 <christianigwebuike30@gmail.com>
2022-10-29 23:26:59 -0700

    first commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -6,14 +6,22 @@
  * @index: The index where the bit get at
  * Return: The value of the bit or -1 if an error occured
  */
+
 int get_bit(unsigned long int n, unsigned int index)
 {
 unsigned long int max = 0x01;
+
 max <<= index;
+
 if (max == 0)
+
 return (-1);
+
 if ((n & max))
+
 return (1);
+
 else
+
 return (0);
 }
