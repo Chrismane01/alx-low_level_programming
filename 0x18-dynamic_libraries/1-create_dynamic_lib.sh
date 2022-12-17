3131ee161337b1d4522d5ce6df809940ca25ef59
Chrismane01 <christianigwebuike30@gmail.com>
2022-12-16 17:32:41 -0800

    first commit

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,8 +1,4 @@
-#!/usr/bin/python3
-gcc -c -fPIC abs.c isalpha.c islower.c memcpy.c putchar.c strcat.c
-strcmp.c strlen.c strncpy.c strspn.c atoi.c  isdigit.c  isupper.c
-memset.c puts.c strchr.c strcpy.c strncat.c strpbrk.c strstr.c
-gcc abs.o isalpha.o islower.o memcpy.o putchar.o strcat.o strcmp.o
-strlen.o strncpy.o strspn.o atoi.o isdigit.o isupper.o memset.o puts.o
-strchr.o strcpy.o strncat.o strpbrk.o strstr.o -shared -o liball.so
-export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH
+#!/bin/bash
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
+gcc -shared -o liball.so *.o
+export LD_LIBRARY_PATH.:$LD_LIBRARY_PATH
